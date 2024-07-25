5c683b304275ea6a02a6b3d59e291a2ee41a7540
McJamweasel <mcjamweasel@msn.com>
2024-07-25 21:43:52 +0100

    initial

--- src/main/java/net/mcjamweasel/moarconcrete/MoarConcrete.java
+++ src/main/java/net/mcjamweasel/moarconcrete/MoarConcrete.java
@@ -2,6 +2,7 @@ package net.mcjamweasel.moarconcrete;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.mcjamweasel.moarconcrete.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,8 +12,8 @@ public class MoarConcrete implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-	
 
-		LOGGER.info("Hello Fabric world! ");
+
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/mcjamweasel/moarconcrete/item/ModItems.java
@@ -0,0 +1,30 @@
+package net.mcjamweasel.moarconcrete.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.mcjamweasel.moarconcrete.MoarConcrete;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries){
+        entries.add(RUBY);
+    }
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, new Identifier(MoarConcrete.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        MoarConcrete.LOGGER.info("Registering mod items for " + MoarConcrete.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup)
+    }
+}
